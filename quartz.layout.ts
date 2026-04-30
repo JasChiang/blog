54aa55f6d24898edb84cc78c056e292be02bde4b
JasChiang <jaschiang@live.com>
2026-04-30 16:32:11 +0800

    footer: replace Quartz defaults with personal links (GitHub, LinkedIn, RSS)

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -17,8 +17,9 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://github.com/JasChiang",
+      LinkedIn: "https://www.linkedin.com/in/jascty/",
+      RSS: "https://jasboughtit.com/blog/index.xml",
     },
   }),
 }
